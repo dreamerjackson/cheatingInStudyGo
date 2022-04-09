2d6f2dadcccc03574739be0e72e4aa868b9db447
zhengjianxun <zhuimengshaonian04@gmail.com>
2022-04-09 15:19:48 +0800

    improve

--- code/8_1_operator_precedence.go
+++ code/8_1_operator_precedence.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(((1 + 2) * 5) / 3)
-	fmt.Println(2 & 2 * 2)
-	fmt.Println(2 & (2 * 2))
+	fmt.Println(((1 + 2) * 5) / 3) // 5
+	fmt.Println((1 + 2*5) / 3)     // 3
+
+	fmt.Println(2 * 2 & 2)        //  4 & 2  100 & 010 -> 0
+	fmt.Println(2 * (2 & 2) << 1) // 8
 }
